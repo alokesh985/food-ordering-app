5da6698b63beba7c96ee0bf52f04f84dba715fbc
alokesh985 <alokesh985@gmail.com>
2022-05-10 12:56:55 +0530

    Minor changes

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,2 @@
-import { legacy_createStore as createStore, compose } from "redux";
-import rootReducer from "./reducers";
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(rootReducer, composeEnhancers());
-
+import store from "./store";
 export default store;
